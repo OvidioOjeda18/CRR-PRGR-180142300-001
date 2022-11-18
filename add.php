a72e25b9fdd6c0143848eabec88ddc3253111496
OvidioOjeda18 <116910950+OvidioOjeda18@users.noreply.github.com>
2022-11-18 11:12:45 -0300

    Update add.php

--- add.php
+++ add.php
@@ -1,1 +1,21 @@
+<?php
 
+require_once 'config.php';
+date_default_timezone_set('America/Argentina/Jujuy');
+$fecha_actual=("Y-m-d h:i:s");
+
+if (!empty($_POST)) {
+	$quiro = $_POST ['quiro'];
+	$btn1 = $_POST['btn1'];
+
+  	$sql = 'INSERT INTO room(quiro, btn1) VALUES (:quiro,:btn1)';
+  	$query = $conn->prepare($sql);
+  	$query->execute([
+		'quiro' => $quiro,
+      'btn1' => $btn1
+
+  	]);
+  }  
+
+  header('Location:emergencias.php');
+?>
